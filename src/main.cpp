6344431a5a07d1100258196fe2e397db69845dc3
colornessW <lw252@users.noreply.github.com>
2026-06-29 23:42:39 +0800

    Initial commit: TM_SkillbooksEnhcancer - remove skill book advancement

---
+++ src/main.cpp
@@ -0,0 +1,61 @@
+#include "SKSE/SKSE.h"
+
+namespace
+{
+	using ActivateFunc_t = bool (*)(RE::TESObjectBOOK*, RE::TESObjectREFR*, RE::TESObjectREFR*, std::uint8_t, RE::TESBoundObject*, std::int32_t);
+
+	ActivateFunc_t OriginalActivate = nullptr;
+
+	bool Hook_Activate(
+		RE::TESObjectBOOK* a_book,
+		RE::TESObjectREFR* a_targetRef,
+		RE::TESObjectREFR* a_activatorRef,
+		std::uint8_t a_arg3,
+		RE::TESBoundObject* a_object,
+		std::int32_t a_targetCount)
+	{
+		auto& flags = a_book->data.flags;
+		const bool wasSkillBook = flags.any(RE::OBJ_BOOK::Flag::kAdvancesActorValue);
+
+		if (wasSkillBook) {
+			flags.reset(RE::OBJ_BOOK::Flag::kAdvancesActorValue);
+		}
+
+		const bool result = OriginalActivate(a_book, a_targetRef, a_activatorRef, a_arg3, a_object, a_targetCount);
+
+		if (wasSkillBook) {
+			flags.set(RE::OBJ_BOOK::Flag::kAdvancesActorValue);
+		}
+
+		return result;
+	}
+
+	void Install()
+	{
+		SKSE::log::info("Installing hooks...");
+
+		REL::Relocation<std::uintptr_t> vtable{ RE::VTABLE_TESObjectBOOK[7] };
+
+		OriginalActivate = stl::unrestricted_cast<ActivateFunc_t>(
+			vtable.write_vfunc(0, stl::unrestricted_cast<std::uintptr_t>(&Hook_Activate)));
+
+		SKSE::log::info("Hooks installed.");
+	}
+}
+
+SKSEPluginInfo(
+	.Name = "TM_SkillbooksEnhcancer",
+	.Author = "Thu'mundus",
+	.Version = { 1, 0, 0, 0 },
+	.RuntimeCompatibility = SKSE::VersionIndependence::AddressLibrary
+);
+
+SKSEPluginLoad(const SKSE::LoadInterface* a_skse)
+{
+	SKSE::Init(a_skse);
+
+	Install();
+
+	SKSE::log::info("TM_SkillbooksEnhcancer loaded.");
+	return true;
+}
